140e63a297370db1b19eccb3ee0b1af69cd36ea3
Paul Warren <paul.warren@emc.com>
2018-01-25 15:28:27 -0800

    use $HOME instead of ~
    [#154196054](https://www.pivotaltracker.com/story/show/#154196054)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- scripts/start_node_plugin_tcp.sh
+++ scripts/start_node_plugin_tcp.sh
@@ -10,5 +10,5 @@ mkdir -p ~/csi_plugins
 rm ~/csi_plugins/csi_local_node.*
 
 
-~/csi_local_node --listenAddr=0.0.0.0:50052 --pluginsPath=~/csi_plugins &
+~/csi_local_node --listenAddr=0.0.0.0:50052 --pluginsPath=$HOME/csi_plugins &
 
