795cb77043aa5cf84fa361dae35b4030ba5ffa93
Amin Jamali <ajamali@pivotal.io>
2018-02-13 12:10:07 -0800

    Modify the windows tasks to be able to run in concourse windows workers
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

---
+++ scripts/ci/local-node-plugin-unit.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+cd csi-local-volume-release
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd src/github.com/jeffpak/local-node-plugin
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
